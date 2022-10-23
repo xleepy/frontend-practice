32ffbe81b4f44667084aa6055c2b73862faa9768
xleepy <ragux0@gmail.com>
2022-10-23 03:44:00 +0200

    Fix fonts, add animations, add more container, add formatting

--- ableton/public/js/index.js
+++ ableton/public/js/index.js
@@ -1,13 +1,23 @@
 (() => {
-  const moreBtn = document.getElementById('more-btn');
-  const moreContainer = document.getElementById('more-container');
+  const moreBtn = document.getElementById("more-btn");
+  const moreContainer = document.getElementById("more-container");
+  const secondaryNav = document.getElementById("secondary-nav");
 
-  moreBtn.addEventListener(
-    'click',
-    (event) => {
-      event.preventDefault();
-      moreContainer.classList.toggle('visible');
+  document.addEventListener(
+    "scroll",
+    () => {
+      if (window.scrollY > 200) {
+        secondaryNav.classList.add("hide");
+      } else if (secondaryNav.classList.contains("hide")) {
+        secondaryNav.classList.remove("hide");
+      }
     },
     true
   );
+
+  moreBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    moreBtn.classList.toggle("opened");
+    moreContainer.classList.toggle("visible");
+  });
 })();
