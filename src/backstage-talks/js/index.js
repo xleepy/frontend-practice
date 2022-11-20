9949494b9580548643f8440abd0bc18cdab83047
xleepy <ragux0@gmail.com>
2022-11-20 23:32:42 +0100

    Add all issues in backstage-talks

---
+++ src/backstage-talks/js/index.js
@@ -0,0 +1,35 @@
+let prevElement;
+let prevListItem;
+let prevId;
+const setVisibleElement = (id) => {
+  if (prevListItem) {
+    prevListItem.classList.remove("selected");
+  }
+
+  if (prevElement) {
+    prevElement.classList.remove("visible");
+    document.body.classList.remove(prevId);
+  }
+
+  const listItem = document.querySelector(`[href='#${id}']`);
+  if (listItem) {
+    listItem.classList.add("selected");
+    prevListItem = listItem;
+  }
+
+  const element = document.getElementById(id);
+  if (element) {
+    prevElement = element;
+    prevId = id;
+    document.body.classList.add(id);
+    element.classList.add("visible");
+  }
+};
+
+window.addEventListener("popstate", () => {
+  const { hash } = location;
+  const id = hash.replace("#", "");
+  setVisibleElement(id);
+});
+
+setVisibleElement("issue6");
